a5e757ed230386413335d857db996154bf92309e
daniilnizamov7-star <daniilnizamov7@gmail.com>
2026-04-22 19:34:07 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,18 +2,12 @@ const CACHE_NAME = 'osoznanie-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
+self.addEventListener('activate', (e) => { e.waitUntil(clients.claim()); });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
 });
